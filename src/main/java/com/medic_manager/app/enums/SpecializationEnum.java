70c9a120a8d6c8f8993e7f975bad3d1b6574e5b7
maniakmichal <maniak.michal@gmail.com>
2024-06-29 15:58:23 +0200

    add specializations

---
+++ src/main/java/com/medic_manager/app/enums/SpecializationEnum.java
@@ -0,0 +1,14 @@
+package com.medic_manager.app.enums;
+
+public enum SpecializationEnum {
+    PEDIATRICIAN,
+    DERMATOLOGIST,
+    SURGEON,
+    PHYSIOTHERAPIST,
+    DENTIST,
+    NEUROLOGIST,
+    CARDIOLOGIST,
+    ORTHOPEDIST,
+    OCULIST,
+    NURSE
+}

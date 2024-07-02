ffae2a0c5d7861098426ac3cbc1de602635a89b1
maniakmichal <maniak.michal@gmail.com>
2024-07-02 21:00:26 +0200

    refactor introduce abstract base entity and user entity

--- src/main/java/com/medic_manager/app/entities/DoctorEntity.java
+++ src/main/java/com/medic_manager/app/entities/DoctorEntity.java
@@ -4,30 +4,12 @@ import com.medic_manager.app.enums.SpecializationEnum;
 import jakarta.persistence.*;
 import org.springframework.validation.annotation.Validated;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "doctor")
 @Validated
-public class DoctorEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false, updatable = false)
-    private Long id;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime created_at;
-
-    @Column(name = "modified_at", nullable = false)
-    private LocalDateTime modified_at;
-
-    @Column(name = "name", nullable = false)
-    private String name;
-
-    @Column(name = "surname", nullable = false)
-    private String surname;
+public class DoctorEntity extends UserEntity {
 
     @ElementCollection(fetch = FetchType.LAZY)
     @Column(name = "specialization", nullable = false)
@@ -37,6 +19,4 @@ public class DoctorEntity {
     @Column(name = "imageUrl")
     private String imageUrl;
 
-    @Column(name = "email", nullable = false)
-    private String email;
 }

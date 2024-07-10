f6d9889df7bd553e019daa42f234e4334a73248f
maniakmichal <maniak.michal@gmail.com>
2024-07-10 19:27:41 +0200

    add to

---
+++ src/main/java/com/medic_manager/app/tos/DoctorTo.java
@@ -0,0 +1,17 @@
+package com.medic_manager.app.tos;
+
+
+import com.medic_manager.app.enums.SpecializationEnum;
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.List;
+
+public record DoctorTo(@Nullable Long id,
+                       @NotBlank String name,
+                       @NotBlank String surname,
+                       @NotBlank String email,
+                       @NotNull List<SpecializationEnum> specializationEnums,
+                       @Nullable String imageUrl) {
+}

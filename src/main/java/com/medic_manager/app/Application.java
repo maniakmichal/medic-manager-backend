c3dcda43f43ae3a2d5e8cec9a4cd9bd2b25691b2
maniakmichal <maniak.michal@gmail.com>
2024-07-11 20:06:03 +0200

    fix config errors

--- src/main/java/com/medic_manager/app/Application.java
+++ src/main/java/com/medic_manager/app/Application.java
@@ -1,4 +1,4 @@
-package src.main.java.com.medic_manager.app;
+package com.medic_manager.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

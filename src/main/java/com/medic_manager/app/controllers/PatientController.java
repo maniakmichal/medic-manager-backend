80fa31a70cadc8c1b493056c5f4ac81a153b8fe9
maniakmichal <maniak.michal@gmail.com>
2024-07-11 19:40:22 +0200

    add controller new method and refactor

--- src/main/java/com/medic_manager/app/controllers/PatientController.java
+++ src/main/java/com/medic_manager/app/controllers/PatientController.java
@@ -1,9 +1,34 @@
 package com.medic_manager.app.controllers;
 
+import com.medic_manager.app.mappers.PatientMapper;
+import com.medic_manager.app.services.PatientService;
+import com.medic_manager.app.tos.PatientTo;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/com/medic-manager/app/")
 public class PatientController {
+
+    private final PatientService patientService;
+    private final PatientMapper patientMapper;
+
+    public PatientController(final PatientService service, PatientMapper mapper) {
+        this.patientService = service;
+        this.patientMapper = mapper;
+    }
+
+    @GetMapping("patients")
+    @ResponseStatus(HttpStatus.OK)
+    public List<PatientTo> getAllPatients() {
+        return patientService.getAllPatients()
+                .stream()
+                .map(patientMapper::toPatientTo)
+                .toList();
+    }
 }

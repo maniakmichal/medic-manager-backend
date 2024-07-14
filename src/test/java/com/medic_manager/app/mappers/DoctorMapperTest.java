d06cc68f327e521840e0b6b26592d23f4511abd3
maniakmichal <maniak.michal@gmail.com>
2024-07-14 20:24:15 +0200

    add doctor mapper test and testdata

---
+++ src/test/java/com/medic_manager/app/mappers/DoctorMapperTest.java
@@ -0,0 +1,32 @@
+package com.medic_manager.app.mappers;
+
+import com.medic_manager.app.entities.DoctorEntity;
+import com.medic_manager.app.testdata.DoctorTestdata;
+import com.medic_manager.app.tos.DoctorTo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class DoctorMapperTest {
+
+    @Autowired
+    private DoctorMapper doctorMapper;
+
+    @Test
+    void returnMappedDoctorEntity() {
+        //given
+        Long id = 1L;
+        String email = "email@email.com";
+        DoctorEntity doctorEntity = DoctorTestdata.mockDoctorEntityWithIdAndEmail(id, email);
+        //when
+        DoctorTo doctorTo = doctorMapper.toDoctorTo(doctorEntity);
+        //then
+        assertThat(doctorTo)
+                .usingRecursiveComparison()
+                .isEqualTo(doctorEntity);
+    }
+
+}

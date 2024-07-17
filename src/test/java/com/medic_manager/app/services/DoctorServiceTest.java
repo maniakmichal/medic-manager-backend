7a6b1d895d5c4bd446631e09d9b8d6b21d9a7be3
maniakmichal <maniak.michal@gmail.com>
2024-07-17 17:22:47 +0200

    add doctor service test

---
+++ src/test/java/com/medic_manager/app/services/DoctorServiceTest.java
@@ -0,0 +1,43 @@
+package com.medic_manager.app.services;
+
+import com.medic_manager.app.entities.DoctorEntity;
+import com.medic_manager.app.repositories.DoctorRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+class DoctorServiceTest {
+
+    @MockBean
+    private DoctorRepo doctorRepo;
+
+    @Autowired
+    private DoctorService doctorService;
+
+    @BeforeEach
+    void setup() {
+        doctorService = new DoctorService(doctorRepo);
+    }
+
+    @Test
+    void returnEmptyListWhenNoDoctorsFound() {
+        //given
+        //when
+        when(doctorRepo.findAll()).thenReturn(List.of());
+        List<DoctorEntity> doctors = doctorService.getAllDoctors();
+        //then
+        assertThat(doctors).isEmpty();
+    }
+
+}

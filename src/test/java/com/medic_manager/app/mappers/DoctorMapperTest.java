ed516e0a3c26fa9018fe8b673641e0eed8395923
maniakmichal <maniak.michal@gmail.com>
2024-07-17 20:13:21 +0200

    fix check

--- src/test/java/com/medic_manager/app/mappers/DoctorMapperTest.java
+++ src/test/java/com/medic_manager/app/mappers/DoctorMapperTest.java
@@ -9,7 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+@SpringBootTest(classes = DoctorMapper.class)
 class DoctorMapperTest {
 
     @Autowired
--- src/test/java/com/medic_manager/app/services/DoctorServiceTest.java
+++ src/test/java/com/medic_manager/app/services/DoctorServiceTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -21,6 +22,7 @@ class DoctorServiceTest {
     @MockBean
     private DoctorRepo doctorRepo;
 
+    @Autowired
     private DoctorService doctorService;
 
     @BeforeEach

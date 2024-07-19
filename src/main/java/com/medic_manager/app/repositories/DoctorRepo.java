d08307e02789bfb8da28943f2b1806423065faad
maniakmichal <maniak.michal@gmail.com>
2024-07-19 19:40:20 +0200

    add guards

--- src/main/java/com/medic_manager/app/repositories/DoctorRepo.java
+++ src/main/java/com/medic_manager/app/repositories/DoctorRepo.java
@@ -4,6 +4,9 @@ import com.medic_manager.app.entities.DoctorEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DoctorRepo extends JpaRepository<DoctorEntity, Long> {
+    List<DoctorEntity> findByEmailIgnoreCase(String email);
 }
--- src/main/java/com/medic_manager/app/services/DoctorService.java
+++ src/main/java/com/medic_manager/app/services/DoctorService.java
@@ -13,6 +13,8 @@ import java.util.logging.Logger;
 @Transactional
 public class DoctorService {
 
+    public static final String CHECKING_IF_TO_INVALID = "Checking if TO invalid.";
+    public static final String ERROR_NULL_OR_INCORRECT_TO_PASSED_AS_ARGUMENT_TO_METHOD = "ERROR: null or incorrect TO passed as argument to method";
     private static final String LIST_ALL_DOCTORS = "List all doctors.";
 
     private final DoctorRepo doctorRepo;
@@ -23,17 +25,44 @@ public class DoctorService {
     }
 
     public DoctorEntity createDoctor(DoctorTo doctorTo) {
-        DoctorEntity doctorEntity = new DoctorEntity();
-        doctorEntity.setName(doctorTo.name());
-        doctorEntity.setSurname(doctorTo.surname());
-        doctorEntity.setEmail(doctorTo.email());
-        doctorEntity.setSpecializationEnums(doctorTo.specializationEnums());
-        doctorEntity.setImageUrl(doctorTo.imageUrl());
-        return doctorRepo.save(doctorEntity);
+        if (isToInvalid(doctorTo)) {
+            logger.info(ERROR_NULL_OR_INCORRECT_TO_PASSED_AS_ARGUMENT_TO_METHOD);
+            throw new IllegalArgumentException(ERROR_NULL_OR_INCORRECT_TO_PASSED_AS_ARGUMENT_TO_METHOD);
+        }
+        List<DoctorEntity> doctorsByEmails = doctorRepo.findByEmailIgnoreCase(doctorTo.email());
+        if (!doctorsByEmails.isEmpty()) {
+            throw new IllegalArgumentException("HERE SHOULD BE SPECIFIC EXCEPTION.");
+        } else {
+            DoctorEntity doctorEntity = new DoctorEntity();
+            doctorEntity.setName(doctorTo.name());
+            doctorEntity.setSurname(doctorTo.surname());
+            doctorEntity.setEmail(doctorTo.email());
+            doctorEntity.setSpecializationEnums(doctorTo.specializationEnums());
+            doctorEntity.setImageUrl(doctorTo.imageUrl());
+            return doctorRepo.save(doctorEntity);
+        }
     }
 
     public List<DoctorEntity> getAllDoctors() {
         logger.info(LIST_ALL_DOCTORS);
         return doctorRepo.findAll();
+    }
+
+    private boolean isToInvalid(DoctorTo doctorTo) {
+        logger.info(CHECKING_IF_TO_INVALID);
+        if (
+                doctorTo == null
+                        || doctorTo.id() != null
+                        || doctorTo.name() == null
+                        || doctorTo.surname() == null
+                        || doctorTo.email() == null
+        ) {
+            return true;
+        } else {
+            return doctorTo.name().isEmpty()
+                    || doctorTo.surname().isEmpty()
+                    || doctorTo.email().isEmpty()
+                    || doctorTo.specializationEnums().isEmpty();
+        }
     }
 }

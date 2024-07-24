2190a61250b044a20569286598316c341f2d3a95
maniakmichal <maniak.michal@gmail.com>
2024-07-24 17:35:26 +0200

    introduce common logger util

---
+++ src/main/java/com/medic_manager/app/common/LoggerTextUtil.java
@@ -0,0 +1,10 @@
+package com.medic_manager.app.common;
+
+public class LoggerTextUtil {
+    public static final String LIST_ALL_DOCTORS = "List all doctors.";
+    public static final String CHECKING_IF_TO_INVALID = "Checking if TO invalid.";
+    public static final String ERROR_NULL_OR_INCORRECT_TO_PASSED_AS_ARGUMENT_TO_METHOD = "ERROR: null or incorrect TO passed as argument to method";
+
+    private LoggerTextUtil() {
+    }
+}

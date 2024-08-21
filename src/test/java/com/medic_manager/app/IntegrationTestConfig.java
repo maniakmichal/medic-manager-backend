d856902f7055c6c3903ba851cd5bb782d9412a0a
maniakmichal <maniak.michal@gmail.com>
2024-08-21 19:11:16 +0200

    add random port for integration tests

--- src/test/java/com/medic_manager/app/IntegrationTestConfig.java
+++ src/test/java/com/medic_manager/app/IntegrationTestConfig.java
@@ -10,7 +10,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public @interface IntegrationTestConfig {
 }

fd71703aa096dabccca31a281da4a4b2d632e022
maniakmichal <maniak.michal@gmail.com>
2025-03-20 12:38:20 +0100

    add patient repo test finding patient by id with appointments and refactor annotation for repositories tests

--- src/test/java/com/medic_manager/app/repositories/DoctorRepoTest.java
+++ src/test/java/com/medic_manager/app/repositories/DoctorRepoTest.java
@@ -1,17 +1,17 @@
 package com.medic_manager.app.repositories;
 
-import com.medic_manager.app.UnitTestConfig;
 import com.medic_manager.app.entities.DoctorEntity;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.Optional;
 
 import static com.medic_manager.app.testdata.DoctorTestdata.mockDoctorEntity;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@UnitTestConfig
+@ActiveProfiles("test")
 @DataJpaTest
 class DoctorRepoTest {
 

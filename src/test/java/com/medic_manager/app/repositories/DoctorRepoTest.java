f36a51874cb9efbad499b8d025fba2e145d0b85d
maniakmichal <maniak.michal@gmail.com>
2025-04-06 14:27:56 +0200

    fix bug with doctor entity constraint

--- src/test/java/com/medic_manager/app/repositories/DoctorRepoTest.java
+++ src/test/java/com/medic_manager/app/repositories/DoctorRepoTest.java
@@ -21,7 +21,7 @@ class DoctorRepoTest {
     @Test
     void findByEmailIgnoreCase() {
         //given
-        DoctorEntity doctor = mockDoctorEntity();
+        DoctorEntity doctor = mockDoctorEntity("email@example.com");
         DoctorEntity savedDoctor = doctorRepo.save(doctor);
         //when
         Optional<DoctorEntity> foundDoctor = doctorRepo.findByEmailIgnoreCase("EmAiL@eXaMpLe.CoM");
@@ -33,7 +33,7 @@ class DoctorRepoTest {
     @Test
     void notFindByEmailIgnoreCaseWhenNotExistingEmail() {
         //given
-        DoctorEntity doctor = mockDoctorEntity();
+        DoctorEntity doctor = mockDoctorEntity("email@example.com");
         doctorRepo.save(doctor);
         //when
         Optional<DoctorEntity> foundDoctor = doctorRepo.findByEmailIgnoreCase("NOT_EXISTING_EMAIL");
@@ -44,7 +44,7 @@ class DoctorRepoTest {
     @Test
     void notFindByEmailIgnoreCaseWhenSearchByNotFullLengthEmail() {
         //given
-        DoctorEntity doctor = mockDoctorEntity();
+        DoctorEntity doctor = mockDoctorEntity("email@example.com");
         doctorRepo.save(doctor);
         //when
         Optional<DoctorEntity> foundDoctor = doctorRepo.findByEmailIgnoreCase("email@example.co");
